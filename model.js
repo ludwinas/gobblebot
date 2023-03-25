50ef236e1a8d9edbe8cd30d817a3575bb9bb0807
Laura Macchini <ludwinas@gmail.com>
2023-03-25 19:05:35 +0100

    persist sqlite database

--- model.js
+++ model.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('sqlite::memory:'); // Example for sqlite
+const sequelize = new Sequelize('database', 'user', 'password', {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	storage: 'database.sqlite',
+});
 
 const Prompt = sequelize.define('prompt', {
     user_id: {
